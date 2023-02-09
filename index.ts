b6d275246f6654422c669d5daa10816df65c388f
Isaac Frank <codingwithisaac@gmail.com>
2023-02-09 18:12:24 +0100

    ability to turn off features from tiktok chatbox

--- index.ts
+++ index.ts
@@ -18,7 +18,9 @@ const MessageEvents = {
 
 let tiktokUsername = "zikyfranky";
 
-let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername, {
+let catSpeakIsOff = {};
+
+let TTKLC = new WebcastPushConnection(tiktokUsername, {
   sessionId: process.env.SESSION_ID,
 });
 
@@ -30,35 +32,39 @@ const main = async () => {
     say.speak(text, "", 1.5);
   };
   try {
-    const state: State = await tiktokLiveConnection.connect();
+    const state: State = await TTKLC.connect();
     console.info(`Connected to roomId ${state.roomId}`);
   } catch (error: any) {
     console.error("Failed to connect");
   }
 
-  tiktokLiveConnection.on(MessageEvents.GIFT, (data: GiftData) => {
+  TTKLC.on(MessageEvents.GIFT, (data: GiftData) => {
     if (data.repeatEnd) {
       const { uniqueId, repeatCount, giftName, diamondCount } = data;
+      if (!catSpeakIsOff[MessageEvents.GIFT]) {
+        speak(
+          `${uniqueId ? uniqueId : "unknown"}, Thanks sooo much for the ${
+            repeatCount > 1 ? pluralize(giftName) : giftName
+          } worth ${diamondCount * repeatCount} ${
+            repeatCount > 1 ? "Diamonds!" : "Diamond"
+          })`
+        );
+      }
+    }
+  });
+
+  TTKLC.on(MessageEvents.LIKE, (data: LikeData) => {
+    const { uniqueId, likeCount } = data;
+    if (!catSpeakIsOff[MessageEvents.LIKE]) {
       speak(
-        `${uniqueId ? uniqueId : "unknown"}, Thanks sooo much for the ${
-          repeatCount > 1 ? pluralize(giftName) : giftName
-        } worth ${diamondCount * repeatCount} ${
-          repeatCount > 1 ? "Diamonds!" : "Diamond"
-        })`
+        `${uniqueId ? uniqueId : "unknown"}, Thanks sooo much for the like${
+          likeCount > 1 ? "s" : ""
+        } `
       );
     }
   });
 
-  tiktokLiveConnection.on(MessageEvents.LIKE, (data: LikeData) => {
-    const { uniqueId, likeCount } = data;
-    speak(
-      `${uniqueId ? uniqueId : "unknown"}, Thanks sooo much for the like${
-        likeCount > 1 ? "s" : ""
-      } `
-    );
-  });
-
-  tiktokLiveConnection.on(MessageEvents.ROOMUSER, async (data: ViewData) => {
+  TTKLC.on(MessageEvents.ROOMUSER, async (data: ViewData) => {
     const now = Date.now();
     const mins = (((now - lastBroadcast) % 86400000) % 3600000) / 60000;
 
@@ -71,14 +77,18 @@ const main = async () => {
             viewer.coinCount > 1 ? "s" : ""
           }`;
         });
-      speak(
-        `Shoutout to my awesome gifters!!!\n\n${lines.join(
-          "\n"
-        )}\n\nAnd thanks to my awsome ${viewerCount} view${
-          viewerCount > 1 ? "ers" : ""
-        }`
-      );
-      const msg = `================================================================\n\t\t\t\t\t\t\t\t🇹​​​​​🇴​​​​​🇵​​​​​ 🇻​​​​​🇮​​​​​🇪​​​​​🇼​​​​​🇪​​​​​🇷​​​​​🇸​​​​​\t\t\t\t\t\t\t\t\n================================================================
+      if (!catSpeakIsOff[MessageEvents.ROOMUSER]) {
+        speak(
+          (topViewers.length > 0
+            ? `Shoutout to my awesome gifters!!!\n\n${lines.join(
+                "\n"
+              )}\n\nAnd t`
+            : "T") +
+            `hanks to my awsome ${viewerCount} view${
+              viewerCount > 1 ? "ers" : ""
+            }`
+        );
+        const msg = `================================================================\n\t\t\t\t\t\t\t\t🇹​​​​​🇴​​​​​🇵​​​​​ 🇻​​​​​🇮​​​​​🇪​​​​​🇼​​​​​🇪​​​​​🇷​​​​​🇸​​​​​\t\t\t\t\t\t\t\t\n================================================================
       ${data.topViewers
         .filter((viewer) => viewer.coinCount > 0)
         .map(
@@ -88,45 +98,65 @@ const main = async () => {
             }`
         )}\n\nThere are ${data.viewerCount} person(s) currently watching`;
 
-      console.log(msg);
-      lastBroadcast = Date.now();
+        console.log(msg);
+        lastBroadcast = Date.now();
+      }
     }
   });
 
-  tiktokLiveConnection.on(MessageEvents.MEMBER, (data: Common) => {
+  TTKLC.on(MessageEvents.MEMBER, (data: Common) => {
     const { uniqueId } = data;
-    speak(`${uniqueId ? uniqueId : "unknown"}!! Thanks for joining the LIVE!`);
+    if (!catSpeakIsOff[MessageEvents.MEMBER]) {
+      speak(
+        `${uniqueId ? uniqueId : "unknown"}!! Thanks for joining the LIVE!`
+      );
+    }
   });
 
-  tiktokLiveConnection.on(MessageEvents.CHAT, (data: ChatData) => {
+  TTKLC.on(MessageEvents.CHAT, (data: ChatData) => {
     const { uniqueId, comment } = data;
-    // speak(`${uniqueId?uniqueId:"unknown"}: ${comment}`);
+    if (uniqueId == "zikyfranky") {
+      // is admin
+      const [cat, value] = comment.split(",");
+      catSpeakIsOff[cat] = parseInt(value);
+      speak(`${cat} audio is ${catSpeakIsOff[cat] ? "off" : "on"}`);
+    } else {
+      const commentArr = comment.split("say@");
+      if (commentArr[0] == "" && !catSpeakIsOff["viewers"]) {
+        speak(`${commentArr[1]}`);
+      }
+    }
     console.log(`${uniqueId ? uniqueId : "unknown"}: ${comment}`);
   });
 
-  tiktokLiveConnection.on(MessageEvents.FOLLOW, (data: Common) => {
+  TTKLC.on(MessageEvents.FOLLOW, (data: Common) => {
     const { uniqueId } = data;
-    speak(
-      `${
-        uniqueId ? uniqueId : "unknown"
-      }!! You are Awesome!!, Thanks for the follow!`
-    );
+
+    if (!catSpeakIsOff[MessageEvents.FOLLOW]) {
+      speak(
+        `${
+          uniqueId ? uniqueId : "unknown"
+        }!! You are Awesome!!, Thanks for the follow!`
+      );
+    }
   });
 
-  tiktokLiveConnection.on(MessageEvents.SHARE, (data: Common) => {
+  TTKLC.on(MessageEvents.SHARE, (data: Common) => {
     const { uniqueId } = data;
-    speak(
-      `${uniqueId ? uniqueId : "unknown"}!! Thanks for the sharing the LIVE`
-    );
+    if (!catSpeakIsOff[MessageEvents.SHARE]) {
+      speak(`${uniqueId ? uniqueId : "unknown"}!! Thanks for sharing the LIVE`);
+    }
   });
 
-  tiktokLiveConnection.on(MessageEvents.SUBSCRIBE, (data: Common) => {
+  TTKLC.on(MessageEvents.SUBSCRIBE, (data: Common) => {
     const { uniqueId } = data;
-    speak(
-      `${uniqueId ? uniqueId : "unknown"}!! OH! MY! GOD!, ${
-        data.uniqueId
-      }. Thanks for SUBSCRIBING!!`
-    );
+    if (!catSpeakIsOff[MessageEvents.SUBSCRIBE]) {
+      speak(
+        `${uniqueId ? uniqueId : "unknown"}!! OH! MY! GOD!, ${
+          data.uniqueId
+        }. Thanks for SUBSCRIBING!!`
+      );
+    }
   });
 };
 
